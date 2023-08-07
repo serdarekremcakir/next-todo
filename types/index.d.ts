aa938e62118644ea8e8594980b29c27f2ed94559
serdarekremcakir <serdarekremcakir@hotmail.com>
2023-08-07 03:48:21 +0300

    fix build

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+declare module '@splidejs/react-splide' {
+    import { ReactNode } from 'react';
+  
+    // Burada @splidejs/react-splide modülünden gelen bileşenlerin tür bildirimlerini oluşturmalısınız
+    // Örnek olarak:
+    export const Splide: any;
+    export const SplideSlide: any;
+  
+    // Eğer daha fazla bileşen varsa onları da bu dosyada bildirebilirsiniz.
+  
+    // Ayrıca, eğer @splidejs/react-splide modülünden fonksiyonlar veya diğer yapılar kullanıyorsanız,
+    // onların da tür bildirimlerini buraya eklemelisiniz.
+  }
+  
